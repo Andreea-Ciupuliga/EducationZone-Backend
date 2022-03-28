48366ae85ad54d8a02441ac61b8efd5a5c2518ac
Andreea-Ciupuliga <80451641+Andreea-Ciupuliga@users.noreply.github.com>
2022-03-28 21:54:04 +0300

    add student id to dto

--- EducationZone-Backend/src/main/java/com/example/EducationZoneBackend/DTOs/StudentDTOs/GetStudentDTO.java
+++ EducationZone-Backend/src/main/java/com/example/EducationZoneBackend/DTOs/StudentDTOs/GetStudentDTO.java
@@ -1,4 +1,5 @@
 package com.example.EducationZoneBackend.DTOs.StudentDTOs;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -9,6 +10,8 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class GetStudentDTO {
+
+    private Long id;
 
     private String firstName;
 
