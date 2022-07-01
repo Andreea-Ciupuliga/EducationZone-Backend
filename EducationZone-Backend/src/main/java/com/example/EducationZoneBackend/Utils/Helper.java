ab7de2f57c89b6dd0f90295c1ace6c0df99ddd8f
Andreea-Ciupuliga <80451641+Andreea-Ciupuliga@users.noreply.github.com>
2022-07-01 18:14:01 +0300

    small changes

--- EducationZone-Backend/src/main/java/com/example/EducationZoneBackend/Utils/Helper.java
+++ EducationZone-Backend/src/main/java/com/example/EducationZoneBackend/Utils/Helper.java
@@ -3,6 +3,8 @@ package com.example.EducationZoneBackend.Utils;
 import org.keycloak.KeycloakPrincipal;
 import org.springframework.security.core.Authentication;
 
+import java.util.Set;
+
 
 public class Helper {
 
@@ -10,6 +12,10 @@ public class Helper {
     //metoda asta ne intoarce username-ul care e encodat in token
     public static String getKeycloakUser(Authentication authentication) {
         return ((KeycloakPrincipal) authentication.getPrincipal()).getKeycloakSecurityContext().getToken().getPreferredUsername();
+    }
+
+    public static Set<String> getKeycloakRole(Authentication authentication) {
+        return ((KeycloakPrincipal) authentication.getPrincipal()).getKeycloakSecurityContext().getToken().getRealmAccess().getRoles();
     }
 
 }
